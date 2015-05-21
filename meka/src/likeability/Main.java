fc3f499584bfa36288461b466dcc6935eee0f685
ela <elzbieta.siwy@gmail.com>
2015-05-21 23:15:33 +0100

    Test filter before testing.

--- meka/src/likeability/Main.java
+++ meka/src/likeability/Main.java
@@ -1,6 +1,7 @@
 package likeability;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -8,21 +9,32 @@ import weka.filters.Filter;
 import weka.filters.unsupervised.instance.RemoveWithValues;
 import weka.core.Attribute;
 import weka.core.Instances;
+import weka.core.converters.ArffSaver;
 
 public class Main {
 	
 	Instances train;
 	Instances test;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+	public static void main(String[] args) throws Exception {
+		BufferedReader reader = new BufferedReader(new FileReader(args[0]));
 		Instances data = new Instances(reader);
 		reader.close();
+		TestFilter filter = new TestFilter();
+		filter.setA(data.attribute("gender"));
+		filter.setB(data.attribute("age"));
+		Instances test = Filter.useFilter(data, filter);
+		filter.setInvert(true);
+		Instances train = Filter.useFilter(data, filter);
+		ArffSaver saver = new ArffSaver();
+		saver.setInstances(train);
+		saver.setFile(new File(args[1]));
+		saver.writeBatch();
 		
+		saver.setInstances(test);
+		saver.setFile(new File(args[2]));
+		saver.writeBatch();
 		
-	}
-	
-	public static void getTest(Instances data) throws Exception {
 	}
 
 }
--- meka/src/likeability/TestFilter.java
+++ meka/src/likeability/TestFilter.java
@@ -8,11 +8,16 @@ import weka.core.Instances;
 import weka.filters.Filter;
 import weka.filters.SimpleBatchFilter;
 import weka.filters.unsupervised.instance.RemoveWithValues;
+import weka.filters.unsupervised.instance.Resample;
 
+@SuppressWarnings("serial")
 public class TestFilter extends SimpleBatchFilter {
 	
 	private Attribute a;
 	private Attribute b;
+	private int sampleSizePercent = 15;
+	private boolean invert = false;
+	private int seed = 1;
 	
 	@Override
 	protected Instances process(Instances inst) throws Exception {
@@ -21,7 +26,8 @@ public class TestFilter extends SimpleBatchFilter {
 		for(Instances data: partitionsA) {
 			partitions.addAll(partition(data, b));
 		}
-		return inst;
+		
+		return getTestSet(partitions);
 	}
 	
 	/*
@@ -39,20 +45,75 @@ public class TestFilter extends SimpleBatchFilter {
 		}
 		return instances;
 	}
+	
+	private Instances getTestSet(List<Instances> insts) throws Exception {
+		Instances output = new Instances(insts.get(0), 0);
+		Resample filter = new Resample();
+		filter.setRandomSeed(seed);
+		filter.setNoReplacement(true);
+		filter.setInvertSelection(invert);
+		filter.setSampleSizePercent(sampleSizePercent);
+		for(Instances inst: insts) {
+			output.addAll(Filter.useFilter(inst, filter));
+		}
+		return output;
+		
+	}
 
 	@Override
-	protected Instances determineOutputFormat(Instances arg0) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	protected Instances determineOutputFormat(Instances arg) throws Exception {
+		return new Instances(arg, 0);
 	}
 
 	@Override
 	public String globalInfo() {
-		// TODO Auto-generated method stub
-		return null;
+		return "A filter which partitions the data so that each partition contains"
+				+ " only instances with one value of attribute a and b, then takes "
+				+ "a random subset of values from each partition and merges them to"
+				+ " produce the final set.";
 	}
 	
 	public static void main(String[] args) {
 	     runFilter(new RemoveWithValues(), args);
-	   }
+	}
+	
+	public Attribute getA() {
+		return a;
+	}
+
+	public void setA(Attribute a) {
+		this.a = a;
+	}
+
+	public Attribute getB() {
+		return b;
+	}
+
+	public void setB(Attribute b) {
+		this.b = b;
+	}
+
+	public int getSampleSizePercent() {
+		return sampleSizePercent;
+	}
+
+	public void setSampleSizePercent(int sampleSizePercent) {
+		this.sampleSizePercent = sampleSizePercent;
+	}
+
+	public boolean isInvert() {
+		return invert;
+	}
+
+	public void setInvert(boolean invert) {
+		this.invert = invert;
+	}
+
+	public int getSeed() {
+		return seed;
+	}
+
+	public void setSeed(int seed) {
+		this.seed = seed;
+	}
 }
